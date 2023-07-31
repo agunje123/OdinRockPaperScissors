ca61f9211a1928bd46fad689a0a35b5258953b44
agunje123 <andrija.gunjevic1@gmail.com>
2023-07-31 16:55:10 +0200

    Add logic for winner of game

--- game.js
+++ game.js
@@ -1,10 +1,20 @@
 //Let's say 0 is paper, 1 is rock and 2 are scissors
-function getComputerSelection() {
-    let result = Math.floor(Math.random() * 3);
-    return result;
+function getComputerResult() {
+  let result = Math.floor(Math.random() * 3);
+  return result;
 }
 
-void function getPlayerSelection(playerSelection) {
-    playGame(playerSelection);
+function playGame(playerSelection) {
+  let computerSelection = getComputerResult();
+  if (playerSelection === computerSelection) {
+    return "It's a tie! Choose again!";
+  } else if (
+    (playerSelection === 1 && computerSelection === 2) ||
+    (playerSelection === 0 && computerSelection === 1) ||
+    (playerSelection === 2 && computerSelection === 0)
+  ) {
+    return "You win!";
+  } else {
+    return "You lose!";
+  }
 }
-
