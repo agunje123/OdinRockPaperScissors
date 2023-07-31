180ad30bdd0422c5766177d03e296d409aeb7f10
agunje123 <andrija.gunjevic1@gmail.com>
2023-07-31 17:57:03 +0200

    Track score of computer and user

--- game.js
+++ game.js
@@ -1,21 +1,14 @@
-document.getElementById("rock").addEventListener("click", function() {
-    playGame(0)
-})
-document.getElementById("paper").addEventListener("click", function () {
-    playGame(1)
-});
-document.getElementById("scissors").addEventListener("click", function () {
-    playGame(2)
-});
+let computerResult = 0;
+let playerResult = 0;
 
 //Let's say 0 is paper, 1 is rock and 2 are scissors
-function getComputerResult() {
+function getComputerSelection() {
   let result = Math.floor(Math.random() * 3);
   return result;
 }
 
-function playGame(playerSelection) {
-  let computerSelection = getComputerResult();
+function playRound(playerSelection) {
+  let computerSelection = getComputerSelection();
   if (playerSelection === computerSelection) {
     return "It's a tie! Choose again!";
   } else if (
@@ -23,8 +16,34 @@ function playGame(playerSelection) {
     (playerSelection === 0 && computerSelection === 1) ||
     (playerSelection === 2 && computerSelection === 0)
   ) {
+    playerResult++;
     return "You win!";
   } else {
+    computerResult++;
     return "You lose!";
   }
 }
+
+function playGame(playerSelection) {
+  console.log(playRound(playerSelection));
+  if (playerResult === 5) {
+    console.log("You win the match!");
+    playerResult = 0;
+    computerResult = 0;
+  }
+  if (computerResult === 5) {
+    console.log("You lose the match!");
+    playerResult = 0;
+    computerResult = 0;
+  }
+}
+
+document.getElementById("rock").addEventListener("click", function () {
+  playGame(0);
+});
+document.getElementById("paper").addEventListener("click", function () {
+  playGame(1);
+});
+document.getElementById("scissors").addEventListener("click", function () {
+  playGame(2);
+});
