88f7ca9f55586b8e772d499c23b1209686e6a5c2
agunje123 <andrija.gunjevic1@gmail.com>
2024-05-14 21:54:19 +0200

    Update score in ui

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ let computerResult = 0;
 let playerResult = 0;
 const playerScore = document.getElementById("playerScore");
 const computerScore = document.getElementById("computerScore");
+const result = document.getElementById("result");
 
 //Let's say 0 is paper, 1 is rock and 2 are scissors
 function getComputerSelection() {
@@ -12,16 +13,18 @@ function getComputerSelection() {
 function playRound(playerSelection) {
 	let computerSelection = getComputerSelection();
 	if (playerSelection === computerSelection) {
-		return "It's a tie! Choose again!";
+		result.textContent = "It's a tie! Choose again!";
 	} else if (
 		(playerSelection === 1 && computerSelection === 2) ||
 		(playerSelection === 0 && computerSelection === 1) ||
 		(playerSelection === 2 && computerSelection === 0)
 	) {
 		playerResult++;
+		result.textContent = "You win! Choose again!";
 		playerScore.textContent = playerResult;
 	} else {
 		computerResult++;
+		result.textContent = "You lose! Choose again!";
 		computerScore.textContent = computerResult;
 	}
 }
@@ -29,14 +32,18 @@ function playRound(playerSelection) {
 function playGame(playerSelection) {
 	playRound(playerSelection);
 	if (playerResult === 5) {
-		console.log("You win the match!");
+		result.textContent = "You win the match!";
 		playerResult = 0;
+		playerScore.textContent = 0;
 		computerResult = 0;
+		computerScore.textContent = 0;
 	}
 	if (computerResult === 5) {
-		console.log("You lose the match!");
+		result.textContent = "You lose the match!";
 		playerResult = 0;
+		playerScore.textContent = 0;
 		computerResult = 0;
+		computerScore.textContent = 0;
 	}
 }
 
